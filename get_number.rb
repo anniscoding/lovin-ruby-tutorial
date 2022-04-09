615e55819dd66537b1ed76f9be244fb702570036
anniscoding <>
2022-04-09 16:00:55 +1000

    add store random number for player

--- get_number.rb
+++ get_number.rb
@@ -1,6 +1,7 @@
 # Get My Number Game
 # Written by: Ann!
 
+#gets players name and greets them
 puts "welcome to 'Get My Number!'"
 print "What's your name?"
 
@@ -9,5 +10,12 @@ input = gets
 name = input.chomp
 puts "Welcome, #{name}!"
 p input
+
+#Store a random number for the player to guess
+puts "Hey I've got a random number"
+puts "can you guess this?"
+targetnumber = rand(100) + 1
+
+#keep track of player guesses
 
 
