0de3f5e51d207bcecd932746e1ba6afe4942f525
anniscoding <>
2022-04-09 15:27:44 +1000

    ruby exercises and building first game

---
+++ get_number.rb
@@ -0,0 +1,13 @@
+# Get My Number Game
+# Written by: Ann!
+
+puts "welcome to 'Get My Number!'"
+print "What's your name?"
+
+input = gets
+
+name = input.chomp
+puts "Welcome, #{name}!"
+p input
+
+
